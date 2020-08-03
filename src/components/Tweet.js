69a76dcf07079d607a7dbb38927861358e6dab7a
Elda Moudy <elda@moudy.net>
2020-08-03 16:50:37 -0700

    problem with icons

---
+++ src/components/Tweet.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { formatTweet } from '../utils/helpers'
+import TiArrowBackOutline from 'react-icons/lib/ti/arrow-back-outline'
+import TiHeartOutline from 'react-icons/lib/ti/heart-outline'
+import TiHeartFullOutline from 'react-icons/lib/ti/heart-full-outline'
+
+ 
+class Tweet extends Component {
+	render() {
+		const { tweet } = this.props
+		
+		if (tweet === null) {
+			return <p>This Tweet doesnt exist</p>
+		}
+
+		const {
+			name, avatar, timestamp, text, hasLiked, likes, replies, id, parent
+		} = tweet
+
+		return (
+			<div className = 'tweet'>
+				<img
+					src = {avatar}
+					alt = {'Avatar of ${name}'}
+					className = 'avatar'
+				/>
+			</div>
+		)
+	}
+}
+
+function mapStateToProps({ authedUser, users, tweets }, { id }){
+	const tweet = tweets[id]
+	const parentTweet = tweet 
+		? [tweet.replyingTo]
+		: null
+
+	return{
+		authedUser, 
+		tweet: tweet
+			? formatTweet(tweet, users[tweet.author], authedUser, parentTweet)
+			: null
+	}
+}
+
+export default connect(mapStateToProps)(Tweet)
